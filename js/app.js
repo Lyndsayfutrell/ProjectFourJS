9780ba6cf0db513aa15173801b5e0e72467f88d9
Lyndsay Futrell <lyndsayfutrell@gmail.com>
2022-10-15 12:10:48 -0400

    added keyboard functionality

--- js/app.js
+++ js/app.js
@@ -16,13 +16,22 @@ buttonReset.addEventListener ('click', (e) => {
 const hearts = document.querySelectorAll('img');
 const qwerty = document.getElementById('qwerty');
 const h2 = document.querySelector('.title');
+const keyboard = document.querySelectorAll("button");
 qwerty.addEventListener('click', e => {
     if(e.target.textContent.length === 1) {
         game.handleInteraction(e.target);
     }
 });
 
-qwerty.addEventListener('keyup', e => {
-    game.handleInteraction(e.target);
-    console.log(e.target);
+document.addEventListener('keydown', e => {
+    if(overlay.style.display != 'flex'){
+    letter = e.key
+    for (let i = 0; i<keyboard.length;i++){
+        if (keyboard[i].textContent === letter && keyboard[i].className !== 'key chosen wrong') {
+            console.log(keyboard[i].className);
+            e = keyboard[i];
+            game.handleInteraction(e);
+            }
+        }
+    }
  });
